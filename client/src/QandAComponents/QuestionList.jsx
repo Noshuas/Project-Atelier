aa46e929a471af07e38b9d9b123cc90ac68c007e
Derek <maloned@uci.edu>
2021-06-07 10:58:14 -0700

    Add setState hook to QuestionList to limit the Question rendering to just 2 questions on initial render

--- client/src/QandAComponents/QuestionList.jsx
+++ client/src/QandAComponents/QuestionList.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Question from './Question.jsx';
 
 const QuestionList = (props) => {
+  const [displayedQs, setDisplayed] = useState(2);
   return (
     <div>
       {props.questions.map((question, index) => {
-        return <Question info={question} key={index}/>;
+        if (index < displayedQs) {
+          return <Question info={question} key={index}/>;
+        }
       })}
     </div>
   );
