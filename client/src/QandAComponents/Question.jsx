6630d728733c0179c232b53df90e33fc487d8cb0
Derek <maloned@uci.edu>
2021-06-07 11:09:05 -0700

    Display information for the Question in the Question component

--- client/src/QandAComponents/Question.jsx
+++ client/src/QandAComponents/Question.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Question = (props) => {
   return (
     <div>
-      Insert Info about questions here
+      <h3>Q:{props.info.question_body} </h3>
+      <p>Helpful? </p><p>Yes ({props.info.question_helpfulness})</p>
+      <div>Answer Component here</div>
     </div>
   );
 };
