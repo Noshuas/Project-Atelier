72dc1afb0d0d0cd91c37b2b9bbdf7c3b10aa266d
Derek <maloned@uci.edu>
2021-06-09 14:53:11 -0700

    Add reporting answers functionality

--- client/src/QandAComponents/Answer.jsx
+++ client/src/QandAComponents/Answer.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import brain from '../ReviewsComponents/brain.jsx';
 import Helpfulness from './Helpfulness.jsx';
+import QAapiCalls from './QandAAPIcalls.js';
 
 function Answer(props) {
+  let [reported, setReported] = useState(false);
+
+  let handleClick = (event) => {
+    event.preventDefault();
+    QAapiCalls.postHelpfullnessFeedback('answers', props.info.answer_id, 'report')
+      .then( (res) => {
+        setReported(true);
+      })
+      .catch( (err) => { console.log('err:', err); });
+  };
+  if (!reported) {
+    return (
+      <div>
+        <span>{props.info.body}</span>
+        <div className="answersToolbar">
+          <span>by {props.info.answerer_name} {brain.getFormatedTimestamp(props.info.date)} </span>
+          <span>|</span>
+          <Helpfulness helpfulness={props.info.helpfulness} QorA='answers' id={props.info.answer_id}/>
+          <span>|</span>
+          <a href="#" onClick={handleClick}>Report</a>
+        </div>
+        <Photos photos={props.info.photos}/>
+      </div>
+    );
+  }
   return (
     <div>
-      <span>{props.info.body}</span>
-      <div className="answersToolbar">
-        <span>by {props.info.answerer_name} {brain.getFormatedTimestamp(props.info.date)} </span>
-        <span>|</span>
-        <Helpfulness helpfulness={props.info.helpfulness} QorA='answers' id={props.info.answer_id}/>
-        <span>|</span>
-        <span>Report</span>
-      </div>
-      <Photos photos={props.info.photos}/>
+      <span>Thank you for reporting this answer.</span>
     </div>
   );
 }
