72a4c6569d9c08b4495946a4f6c60027afc24e8a
Luka Petricic <lukapetricic@gmail.com>
2021-06-09 10:19:54 -0700

    Moved Characteristics into seperate file, started on new Interactive Stars component

--- client/src/ReviewsComponents/AddReview.jsx
+++ client/src/ReviewsComponents/AddReview.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import GenericModal from '../QandAComponents/GenericModal.jsx';
 import brain from './brain.jsx';
 import RandRAPIcalls from './RandRAPIcalls';
+import InteractiveStars from './InteractiveStars.jsx';
+import Characteristics from './Characteristics.jsx';
 
 function AddReview(props) {
   let [open, setOpen] = useState(false);
@@ -23,6 +25,7 @@ function AddReview(props) {
           <h4>About the {props.productName}</h4>
           <form>
             <div>
+              <InteractiveStars />
               <div>Do you recommend this product?* </div>
               <label htmlFor='recommend'>Yes</label>
               <input type='radio' name='recommend' required></input>
@@ -60,50 +63,3 @@ function AddReview(props) {
 }
 
 export default AddReview;
-
-//-------------------------------------------------
-
-let charsTable = {
-  Size: ['A size too small', '½ a size too small', 'Perfect', '½ a size too big', 'A size too wide'],
-  Width: ['Too narrow', 'Slightly narrow', 'Perfect', 'Slightly wide', 'Too wide'],
-  Comfort: ['Uncomfortable', 'Slightly uncomfortable', 'Ok', 'Comfortable', 'Perfect'],
-  Quality: ['Poor', 'Below average', 'What I expected', 'Pretty great', 'Perfect'],
-  Length: ['Runs Short', 'Runs slightly short', 'Perfect', 'Runs slightly long', 'Runs long'],
-  Fit: ['Runs tight', 'Runs slightly tight', 'Perfect', 'Runs slightly long', 'Runs long']
-};
-
-
-
-function Characteristics(props) {
-  let currentChars = Object.keys(props.characteristics);
-
-  return currentChars.map((char, index) => <OneChar char={char} key={index}
-    charValues={props.charValues} setCharValues={props.setCharValues} />);
-}
-
-function OneChar(props) {
-  let char = props.char;
-  const [desc, setDesc] = useState(0)
-
-  function handleClick(e) {
-    let newObj = { ...props.charValues };
-    newObj[char] = e.target.value;
-    props.setCharValues(newObj);
-    setDesc(e.target.value);
-  }
-
-  return (
-    <div className="single-char-input">
-      <div className="current-selected">{desc ? charsTable[char][desc - 1] : 'None selected'}</div>
-      <label htmlFor={char}>{char}:* </label>
-      <div className="char-select-container">
-        {[1, 2, 3, 4, 5].map(num =>
-          <div className="char-uni-selector" key={num}>
-            <input type='radio' name={char} value={num} onClick={handleClick} required></input><br></br>
-            <label htmlFor={char}>{charsTable[char][num - 1]}</label>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
---
+++ client/src/ReviewsComponents/InteractiveStars.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import brain from './brain.jsx';
+import RandRAPIcalls from './RandRAPIcalls';
+
+function InteractiveStars (props) {
+  return (
+    <div>
+      *****
+    </div>
+  );
+}
+
+export default InteractiveStars;
