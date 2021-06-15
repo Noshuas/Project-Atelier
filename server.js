a0bc24acf7f74e7cc345d11fa41528924e27fd72
Luka Petricic <lukapetricic@gmail.com>
2021-06-15 12:39:30 -0700

    Fixed rendering for RandR

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post('/image-upload', (req, res) => {
 
 //Derek's endpoints
 app.get('/qa/questions', (req, res) => {
-  console.log(req.query);
+  console.log('Getting questions');
   let params = {
     product_id: req.query.productId,
     page: 1,
@@ -97,7 +97,7 @@ app.get('/qa/questions', (req, res) => {
 });
 
 app.post('/qa/questions', (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
 
   let headers = config.auth.headers;
   return axios.post(config.url + '/qa/questions', req.body, { headers })
@@ -110,7 +110,7 @@ app.post('/qa/questions', (req, res) => {
 });
 
 app.get('/qa/answers', (req, res) => {
-  //console.log(req.query.question_id);
+  console.log('Getting answers');
   let params = {
     question_id: req.query.question_id,
     page: 1,
