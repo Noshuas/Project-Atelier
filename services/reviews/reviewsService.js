c1cd6ab1d14dc28b200d3cb8eeed6035b0e4afdb
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-06-24 02:22:04 -0700

    Successfully built connection to postgres db for reviews service

--- services/reviews/reviewsService.js
+++ services/reviews/reviewsService.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
 
 let port = 1234;
+let client = new Client('postgres://postgres:password@project-catwalk_db_1:5432');
+
+client.connect((err, success) => {
+  if (success) {
+    console.log('Connected to postgres DB!');
+  } else if (err) {
+    console.log('Failed to connect to postgres DB, for: ');
+    console.error(err);
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.send('Responding to GET request at /');
