84a6464a5b05ad884b497bb0672cfea88b861644
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-06-24 01:37:52 -0700

    Wrote basic server for reviews service

---
+++ services/reviews/reviewsService.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+let port = 1234;
+
+app.get('/', (req, res) => {
+  res.end('Responding to GET request at /');
+});
+
+app.listen(port, () => {
+  console.log(`REVIEWS SERVICE LISTENING AT PORT ${port}`);
+});
